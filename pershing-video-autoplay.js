5c001cc2ef9bc5f24e372aa0749f823fbecddcb4
Pymarketor <marketing@prestigiousyachting.com>
2025-06-27 17:57:29 +0200

    Delete pershing-video-autoplay.js

--- pershing-video-autoplay.js
+++
@@ -1,9 +0,0 @@
-const video = document.getElementById('PershingCharterVideo');
-const section = document.getElementById('Testimonials');
-const observer = new IntersectionObserver(entries => {
-  entries.forEach(entry => {
-    if (entry.isIntersecting) video.play();
-    else video.pause();
-  });
-}, { threshold: 0.5 });
-observer.observe(section);
