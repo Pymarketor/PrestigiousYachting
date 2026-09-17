240f6e90707448dcf00cc55b277f3884acac7391
Pymarketor <marketing@prestigiousyachting.com>
2026-09-17 18:10:59 +0200

    Fix Similar external-link arrow orientation

--- similarsectionv1.js
+++ similarsectionv1.js
@@ -210,6 +210,10 @@
         display: none !important;
       }
 
+      ${ROOT_SELECTOR}[data-py-coverflow-ready="true"] .bottom-right.r-z-45:has([data-py-icon="external-link"]) {
+        transform: none !important;
+      }
+
       ${ROOT_SELECTOR}[data-py-coverflow-ready="true"] .card-favorite-yacht-image > .open-arrow {
         z-index: 12 !important;
         display: flex !important;
