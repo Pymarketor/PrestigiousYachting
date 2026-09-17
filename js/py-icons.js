784bd7336bdcc30b4b3e233632726c662bc8ec99
Pymarketor <marketing@prestigiousyachting.com>
2026-09-17 17:55:02 +0200

    Lock external-link to a consistent 45-degree direction

--- js/py-icons.js
+++ js/py-icons.js
@@ -6,7 +6,7 @@
   'use strict';
 
   var repo = 'Pymarketor/PrestigiousYachting';
-  var version = 'main';
+  var version = 'a0c84bc7924e4e567b6d4be73c46706d9bd6640d';
   var baseUrl = 'https://raw.githubusercontent.com/' + repo + '/' + version + '/icons/';
   var fallbackBaseUrl = 'https://cdn.jsdelivr.net/gh/' + repo + '@' + version + '/icons/';
   var cache = Object.create(null);
@@ -68,7 +68,7 @@
   }
 
   var style = document.createElement('style');
-  style.textContent = '.py-icon{display:inline-flex;width:1.25rem;height:1.25rem;line-height:1;color:var(--py-icon-color,currentColor)}.py-icon-wrap.py-icon{width:var(--py-icon-size,2.5rem)!important;height:var(--py-icon-size,2.5rem)!important}.py-icon-wrap.py-icon>.py-icon-svg{width:var(--py-icon-inner-size,1.25rem)!important;height:var(--py-icon-inner-size,1.25rem)!important}.py-icon-svg{display:block;width:100%;height:100%;color:var(--py-icon-color,#333)!important;stroke:var(--py-icon-color,#333)!important;overflow:visible}.py-icon-svg path,.py-icon-svg line,.py-icon-svg polyline,.py-icon-svg polygon{stroke:currentColor}.py-icon--left{transform:rotate(180deg)}.py-icon--up{transform:rotate(-90deg)}.py-icon--down{transform:rotate(90deg)}';
+  style.textContent = '.py-icon{display:inline-flex;width:1.25rem;height:1.25rem;line-height:1;color:var(--py-icon-color,currentColor)}.py-icon-wrap.py-icon{width:var(--py-icon-size,2.5rem)!important;height:var(--py-icon-size,2.5rem)!important}.py-icon-wrap.py-icon>.py-icon-svg{width:var(--py-icon-inner-size,1.25rem)!important;height:var(--py-icon-inner-size,1.25rem)!important}.py-icon-svg{display:block;width:100%;height:100%;color:var(--py-icon-color,#333)!important;stroke:var(--py-icon-color,#333)!important;overflow:visible}.py-icon-svg path,.py-icon-svg line,.py-icon-svg polyline,.py-icon-svg polygon{stroke:currentColor}[data-py-icon="external-link"]>.py-icon-svg{transform:none!important}.open-arrow[data-py-icon="external-link"],.open-arrow:has([data-py-icon="external-link"]),.open-arrow-link[data-py-icon="external-link"],.open-arrow-link:has([data-py-icon="external-link"]),.arrow-open[data-py-icon="external-link"],.arrow-open:has([data-py-icon="external-link"]),.arrow-open-link[data-py-icon="external-link"],.arrow-open-link:has([data-py-icon="external-link"]),.arrow-open-other[data-py-icon="external-link"],.arrow-open-other:has([data-py-icon="external-link"]){transform:none!important}.py-icon--left{transform:rotate(180deg)}.py-icon--up{transform:rotate(-90deg)}.py-icon--down{transform:rotate(90deg)}';
   document.head.appendChild(style);
 
   window.PYIcons = { render: render, load: load, baseUrl: baseUrl, fallbackBaseUrl: fallbackBaseUrl, markIconNodes: markIconNodes };
